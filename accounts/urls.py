39e47c09e90e4067c1c527bef2f40aae3a5da145
guscsh <guschoish@gmail.com>
2026-06-07 01:02:28 +0800

    已經整好哂眼見有爛既野，fine tune 左啲 routing，整左個 templates/accounts/profile.html 頂當做 Testing，要入番內容

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('register/', views.register_view, name="register"),
+    path('login/', LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path('logout/', views.logout_view, name="logout"),
+    path('profile/', views.profile, name="profile"),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -8,11 +8,14 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request,user)
-            return redirect("shop_list")
+            return redirect("pages:shop_list")
     else:
         form = RegisterForm()
     return render(request,"accounts/register.html",{"form":form})
 
 def logout_view(request):
     logout(request)
-    return redirect("shop_list")
+    return redirect("pages:shop_list")
+
+def profile(request):
+    return render(request,"accounts/profile.html")
