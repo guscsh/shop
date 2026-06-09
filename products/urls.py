df34419ba5518e433bca9b4c74dad558fe67c9b2
claptrap915W <claptrap915@gmail.com>
2026-06-09 19:29:23 +0800

    Reapply "Merge branch 'productsList'"
    This reverts commit 39e640e78d0d4f3cb5768b1403e7b6f532921f2d.

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,5 @@ app_name = 'products' # 對應 models.py 裡面的 reverse('products:detail')
 # 這個變數名稱絕對不能拼錯！
 urlpatterns = [
     path('', views.product_list, name='list'),
-    path('<slug:slug>/', views.product_pages, name='pages'),
+    path('<slug:slug>/', views.product_detail, name='detail'),
 ]
